068581e180b0b5bf31994dd111f2e2ba02a4e20c
Yimkeul <leesjun29@gmail.com>
2025-04-25 18:19:01 +0900

    [Feat] #1 : UIColor Extension 추가

---
+++ TeamProject/TeamProject/Utility/Extensions/UIColor+.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+.swift
+//  TeamProject
+//
+//  Created by yimkeul on 4/25/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static func asset(_ asset: Asset) -> UIColor {
+        guard let color = UIColor(named: asset.rawValue) else {
+            fatalError("Color asset '\(asset.rawValue)' not found in Asset Catalog")
+        }
+        return color
+    }
+    enum Asset: String {
+        case main = "Main"
+        case white = "White"
+        case gray0 = "Gray#0"
+        case gray1 = "Gray#1"
+        case gray2 = "Gray#2"
+        case gray3 = "Gray#3"
+    }
+}
