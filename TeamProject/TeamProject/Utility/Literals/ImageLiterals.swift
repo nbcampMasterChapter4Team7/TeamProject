b4267d78b3d8a3fbe189e6503545babf85df1e51
Yimkeul <leesjun29@gmail.com>
2025-04-25 18:19:57 +0900

    [Feat] #1 - ImageLiterals  추가

---
+++ TeamProject/TeamProject/Utility/Literals/ImageLiterals.swift
@@ -0,0 +1,14 @@
+//
+//  ImageLiterals.swift
+//  TeamProject
+//
+//  Created by yimkeul on 4/25/25.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    static var kickboard: UIImage { .load(name: "kickboard").withRenderingMode(.alwaysOriginal) }
+    static var ping: UIImage { .load(name: "ping").withRenderingMode(.alwaysOriginal) }
+    static var location: UIImage { .load(name: "location").withRenderingMode(.alwaysOriginal) }
+}
