06bbc4f74bcc2aa9d087a06fdb94927eca1a3840
Yimkeul <leesjun29@gmail.com>
2025-04-28 17:42:56 +0900

    [Feat] #10 - 현 위치 바로가기 버튼 추가 (현재는 임시 데이터로 이동)

--- TeamProject/TeamProject/View/RentView/RentViewController.swift
+++ TeamProject/TeamProject/View/RentView/RentViewController.swift
@@ -9,30 +9,52 @@ import UIKit
 
 import SnapKit
 import Then
+import KakaoMapsSDK
 
-final class RentViewController: UIViewController {
+final class RentViewController: KakaoMapViewController {
+
+    private let locationButton = UIButton().then {
+        let img = ImageLiterals.location.resize(newWidth: 32)
+        $0.setImage(img, for: .normal)
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 25
+    }
     
-    let centerLabel = UILabel().then {
-        $0.text = "Test1"
+    override func loadView() {
+        super.loadView()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUI()
-        setAttribute()
+        setLayout()
     }
 
-    func setUI() {
-        self.view.backgroundColor = .red
-        self.view.addSubview(centerLabel)
+    // 엔진이 준비·활성화된 직후에 지도 추가
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addViews()
     }
 
-    func setAttribute() {
-
-        centerLabel.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+    private func setLayout() {
+        view.addSubview(locationButton)
+        locationButton.snp.makeConstraints {
+            $0.width.height.equalTo(53)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
         }
+        locationButton.addTarget(self, action: #selector(didTapLocationButton), for: .touchUpInside)
+        view.bringSubviewToFront(locationButton)
+    }
+
+    @objc private func didTapLocationButton() {
+        let point = MapPoint(longitude: 126.9763, latitude: 37.5867)
+        guard let mapView = mapController?.getView("mapview") as? KakaoMap else {
+            print("지도 뷰를 찾을 수 없습니다.")
+            return
+        }
+        let cameraUpdate = CameraUpdate.make(target: point, mapView: mapView)
+        mapView.moveCamera(cameraUpdate)
+        print("지도 중심을 (37.5867, 126.9763)로 이동했습니다.")
     }
 }
 
