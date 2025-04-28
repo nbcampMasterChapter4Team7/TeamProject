aaecdd66241b0b2324c0d96b0a5ef52a95c8aad2
LEEBUYONG <dqd001@naver.com>
2025-04-28 12:51:28 +0900

    [Feat] #5 - LoginView UI요소 설정
    1. LoginView UI 요소 배치와 제약 설정
    2. TextFieldDelegate 사용 및 확정하여 입력값의 제한을 설정할 수 있도록 함(아이디: 영어, 숫자만 가능, 비밀번호: 영어,숫자,비밀번호만 가능)
    3. Keyboard+를 통해 키보드 배치에 따른 크기 설정 및 return을 누르면 키보드가 내려가도록 설정

--- TeamProject/TeamProject/ViewController/LoginViewController.swift
+++ TeamProject/TeamProject/ViewController/LoginViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
+    private var loginView = LoginView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLoginView()
+        addKeyboardObserver() // 키보드 확장 옵저버 사용
+        setupNavigationBar()
     }
 
-
+    func setupLoginView() {
+        view.addSubview(loginView)
+            
+        loginView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    // memebershipView의 네비게이션 뒤로가기 타이틀 없애기
+    private func setupNavigationBar() {
+        navigationItem.backButtonTitle = ""
+    }
+    
+    // 키보드 확장 옵저버 종료
+    deinit {
+        removeKeyboardObserver()
+    }
+    
+    
 }
-
