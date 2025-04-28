d3556c7d0ec73331c6f625486ee160ffd4d224f3
tls427wodnr <tlswodnr427@naver.com>
2025-04-28 11:46:23 +0900

    [Fix]: 카카오 지도 API 403 Failed 문제 해결중

--- TeamProject/TeamProject/ViewController.swift
+++ TeamProject/TeamProject/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import KakaoMapsSDK
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, MapControllerDelegate {
+
+    private var mapContainer: KMViewContainer!
+    private var mapController: KMController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapContainer = KMViewContainer(frame: view.bounds)
+        mapContainer.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(mapContainer)
+
+        mapController = KMController(viewContainer: mapContainer)
+        mapController.delegate = self
+
+        mapController.prepareEngine()
     }
 
+    func authenticationSucceeded() {
+        let center = MapPoint(longitude: 127.0276, latitude: 37.4979)
+        let mapViewInfo = MapviewInfo(viewName: "mapview", viewInfoName: "map", defaultPosition: center, defaultLevel: 7)
+        mapController.addView(mapViewInfo)
+    }
 
+    func authenticationFailed(_ errorCode: Int, desc: String) {
+        print("인증 실패: \(errorCode) - \(desc)")
+    }
+
+    func addViewSucceeded(_ viewName: String, viewInfoName: String) {
+        if let mapView = mapController.getView(viewName) as? KakaoMap {
+            mapView.viewRect = mapContainer.bounds
+        }
+    }
+    
+    func addViews() {
+        
+    }
 }
-
