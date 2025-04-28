38a6d0ca062594f1e22fa7da1f066eba43d3888b
Yimkeul <leesjun29@gmail.com>
2025-04-28 17:42:20 +0900

    [Feat] #10 - SizeLiterals 추가 (기기별 대응 가능)

--- TeamProject/TeamProject/Utility/Literals/SizeLiterals.swift
+++ TeamProject/TeamProject/Utility/Literals/SizeLiterals.swift
@@ -10,7 +10,11 @@ import UIKit
 struct SizeLiterals {
     struct Screen {
         static let screenWidth: CGFloat = UIScreen.main.bounds.width
+        /// 사용법 :  $0.height.equalTo(SizeLiterals.Screen.screenHeight * 25 / 402)
+        /// ==> 기기별 넓이 25사이즈 대응
         static let screenHeight: CGFloat = UIScreen.main.bounds.height
+        /// 사용법 :  $0.height.equalTo(SizeLiterals.Screen.screenHeight * 25 / 874)
+        /// ==> 기기별 높이 25사이즈 대응
         static let deviceRatio: CGFloat = screenWidth / screenHeight
 
         static let isSmallDevice: Bool = min(screenWidth, screenHeight) <= 375
